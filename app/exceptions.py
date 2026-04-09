dfa339d26bcc8d04e22f3d3153def8ebf0d440c3
Jonatan Valdés Carpio <131415087+jonatanvc@users.noreply.github.com>
2026-04-09 08:28:13 -0400

    Add files via upload

---
+++ app/exceptions.py
@@ -0,0 +1,14 @@
+class PasswordRequiredError(Exception):
+    pass
+
+
+class InvalidPasswordError(Exception):
+    pass
+
+
+class MissingRarBackendError(Exception):
+    pass
+
+
+class Missing7zBackendError(Exception):
+    pass
